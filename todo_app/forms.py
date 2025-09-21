73ddba48e8bc7c5501196143e50bc9bebb7a871f
lleanamariflor <lleanamariflor@gmail.com>
2025-09-21 22:22:56 +0800

    Styled task templates and added base template

---
+++ todo_app/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'completed']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Enter a task...'})
+        }
